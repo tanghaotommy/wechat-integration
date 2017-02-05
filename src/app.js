00bd852533689b3169b2bc64f95b61898d058fc6
汤豪 <tanghao@s-MacBook-Pro.local>
2017-02-05 12:51:19 -0800

    Fixed bugs

--- src/app.js
+++ src/app.js
@@ -30,34 +30,16 @@ app.use('/wechat', wechat(config, function (req, res, next) {
   var message = req.weixin;
   //res.reply("hehe");
   console.log(message);
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
+  if (message.MsgType == 'location') {
+      console.log('location message: ', message.Label);   
+  }
+  if (message.MsgType == 'text') {
     var text = message.Content;
-    console.log(message.Content);
+    console.log('text message: ', message.Content);
     let apiaiRequest = apiAiService.textRequest(text,
     {
       sessionId: message.FromUserName
-    })
+    });
 
     apiaiRequest.on('response', function(response)
     {
@@ -74,10 +56,10 @@ app.use('/wechat', wechat(config, function (req, res, next) {
       //   url: 'http://nodeapi.cloudfoundry.com/'
       // }
       // ]);
-    });
+      });
 
-    apiaiRequest.on('error', (error) => console.error(error));
-    apiaiRequest.end();
+      apiaiRequest.on('error', (error) => console.error(error));
+      apiaiRequest.end();
     // 回复高富帅(图文回复)
   }
 }));
