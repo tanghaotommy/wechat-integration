c0899e66466b65757dea53ffc87aed9d77c43869
汤豪 <tanghao@s-MacBook-Pro.local>
2017-02-24 23:20:02 -0800

    Fix bugs

--- src/app.js
+++ src/app.js
@@ -259,6 +259,7 @@ app.use('/wechat_service', wechat(service_config, function (req, res, next) {
       res.reply(responseText);
     }
     if (message.Event == 'LOCATION') {
+
       data = {"user_id": message.FromUserName,
       "event": "location",
       "latitude": message.Latitude,
